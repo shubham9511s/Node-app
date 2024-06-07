0c21d21794ea71fb458330898789bee0679a3b9e
Shubham Shinde <166897994+shubham9511s@users.noreply.github.com>
2024-06-07 13:58:16 +0530

    Update App.jsx

--- node-project/src/App.jsx
+++ node-project/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This Code is Deploy on ARGO Cd throuth out Jenkins CI
       </p>
     </>
   )
